7bcbf4dbdc4841ac558365648c2d0f606304df98
Masashi Nohara <chancy-m.n-72@ruri.waseda.jp>
2018-11-05 16:31:34 +0900

    experimented controller generation

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get create" do
+    get pages_create_url
+    assert_response :success
+  end
+
+end
